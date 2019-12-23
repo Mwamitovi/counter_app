7f37fed548990d29db717091e6685dd4724bc8de
Martin Matovu <matovu.synergy@gmail.com>
2019-12-23 11:10:30 +0300

    Refactor RichText widget: add style props to it's TextSpan object

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -92,7 +92,31 @@ class ContainerWithBoxDecorationWidget extends StatelessWidget {
           ),
           child: Center(
             child: RichText(
-              text: TextSpan(text: 'Container'),
+              text: TextSpan(
+                text: 'Flutter World',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.deepPurple,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.deepPurpleAccent,
+                  decorationStyle: TextDecorationStyle.dotted,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.normal,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' for',
+                  ),
+                  TextSpan(
+                    text: ' Mobile',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
